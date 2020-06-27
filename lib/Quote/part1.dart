b67d773503dccca6414d24a65b97a99aa8b28e1b
Your Nameshovael <shuvi97@gmail.com>
2020-06-28 00:48:52 +0300

    1

--- lib/Quote/part1.dart
+++ lib/Quote/part1.dart
@@ -13,9 +13,15 @@ class Quate {
 
 String proper(String quate) {
   String result;
+  quate = quate.toLowerCase();
   for (int a = 0; a < quate.length; a++) {
-    // if(quate.substring(0, 1) > 0)
+    if ((quate.substring(a, a + 1).codeUnitAt(0) > 96 &&
+            quate.substring(a, a + 1).codeUnitAt(0) < 123) ||
+        quate.substring(a, a + 1).codeUnitAt(0) == 32 ||
+        quate.substring(a, a + 1).codeUnitAt(0) == 39)
+      result = result + quate.substring(a, a + 1);
   }
+  return result;
 }
 
 fetch_forismatic() async {
@@ -24,9 +30,10 @@ fetch_forismatic() async {
   if (response.statusCode == 200) {
     var map1 = json.decode(response.body);
     String edit = map1[0]['content']['rendered'];
-    // edit = proper(edit);
+    edit = proper(edit);
     print(map1[0]['title']['rendered']);
     print(edit);
+    print(edit.codeUnitAt(1));
   } else
     print(2);
 }
