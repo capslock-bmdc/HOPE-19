f27ec300e24ee1af896d61cb25130a3877551766
zvikarp <zvi@zektec.com>
2020-06-28 00:03:42 +0300

    fixed the button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,26 +11,29 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Widget _cards() {
-    return SwipeStack(
-      children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((int index) {
-        return SwiperItem(builder: (SwiperPosition position, double progress) {
-          return CardWidget(text: index.toString());
-        });
-      }).toList(),
-      visibleCount: 3,
-      stackFrom: StackFrom.Top,
-      translationInterval: 20,
-      scaleInterval: 0.04,
-      onEnd: () => debugPrint("onEnd"),
-      onSwipe: (int index, SwiperPosition position) =>
-          debugPrint("onSwipe $index $position"),
-      onRewind: (int index, SwiperPosition position) =>
-          debugPrint("onRewind $index $position"),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 128),
+      child: SwipeStack(
+        children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((int index) {
+          return SwiperItem(builder: (SwiperPosition position, double progress) {
+            return CardWidget(text: index.toString());
+          });
+        }).toList(),
+        visibleCount: 3,
+        stackFrom: StackFrom.Top,
+        translationInterval: 20,
+        scaleInterval: 0.04,
+        onEnd: () => debugPrint("onEnd"),
+        onSwipe: (int index, SwiperPosition position) =>
+            debugPrint("onSwipe $index $position"),
+        onRewind: (int index, SwiperPosition position) =>
+            debugPrint("onRewind $index $position"),
+      ),
     );
   }
 
   Widget _button() {
-    return RaisedButton(
+    return FloatingActionButton(
       onPressed: () {},
       child: Text("click me"),
     );
@@ -40,12 +43,9 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          _button(),
-          _cards(),
-        ],
-      )),
+        child: _cards(),
+      ),
+      floatingActionButton: _button(),
     );
   }
 }
