b041a3154db4887c6019e9ea600cbea918da80c5
Your Nameshovael <shuvi97@gmail.com>
2020-06-27 23:52:47 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      // home: MyHomePage(title: 'We love bananas!'),
-    );
-=======
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Home());
->>>>>>> 216fbc750f5541e95cb75a559a45b91f896a31bf
   }
 }
